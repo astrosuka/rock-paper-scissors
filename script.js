6a5f6984d81298559521b4e234eade4d546680c3
astrosuka <yegresvoselok@gmail.com>
2023-12-14 17:39:40 -0300

    Make game function work

--- script.js
+++ script.js
@@ -73,30 +73,24 @@ function game(){
     let computerScore = 0;
  
     for (i = 0; i < 5;){
+        console.log("round "+ (i + 1));
         let computerSelection = getComputerChoice();
         let playerSelection = prompt("round " + (i + 1));
         let result = playRound(playerSelection, computerSelection);
 
         if (result === "win") {
-            console.log(result);
             playerScore += 1;
             i += 1;
-
         } else
         if (result === "lose"){
-            console.log(result);
             computerScore += 1;
             i += 1;
-        } 
-        else {
-            console.log(result);
         }
 
-        if (i == 4) {
+        if (i == 5) {
             alert("your score: " + playerScore + '. computer score: ' + computerScore)
         }
     }
-
 }
 
 game();
