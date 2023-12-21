06d0611519e4462a6ee354b96df5e6a6df4004d0
astrosuka <yegresvoselok@gmail.com>
2023-12-21 10:04:09 -0300

    Simplify getComputerChoice function

--- script.js
+++ script.js
@@ -1,51 +1,16 @@
-//randomly return 'Rock', 'Paper' or 'Scissors'
 function getComputerChoice (){
     let randomNumber;
     let computerChoice;
-    //generate random number from 0 to 100
-    randomNumber = Math.floor(Math.random() * 100);
-    if (randomNumber <= 33){
+    randomNumber = Math.floor(Math.random() * 3);
+    if (randomNumber === 0){
         computerChoice = 'Rock';
-    } else if (randomNumber > 33 && randomNumber < 67) {
+    } else if (randomNumber === 1){
         computerChoice = 'Paper';
-    } else {
+    } else if (randomNumber === 2){
         computerChoice = 'Scissors';
     }
     return computerChoice;
 }
-
-//function that plays a single round, should take 2  parameters: playerSelection and computerSelection.
-// function playRound(playerSelection, computerSelection) {
-//     //make playerSelection case insensitive
-//     playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
-//     console.log("your choice: " + playerSelection);
-//     console.log("computer choice: " + computerSelection);
-//     //
-//     if (playerSelection === "Rock" && computerSelection === "Paper"){
-//         return "You Lose! Paper beats Rock";
-//     } else 
-//     if (playerSelection === "Scissors" && computerSelection === "Paper"){
-//         return "You Win! Scissors beats Paper";
-//     } else
-//     if (playerSelection === "Scissors" && computerSelection === "Rock"){
-//         return "You Lose! Rock beats Scissors";
-//     } else
-//     if (playerSelection === "Paper" && computerSelection === "Rock"){
-//         return "You Win! Paper beats Rock";
-//         } else
-//     if (playerSelection === "Rock" && computerSelection === "Scissors"){
-//         return "You Win! Rock beats Scissors";
-//     } else
-//     if (playerSelection === "Paper" && computerSelection === "Scissors"){
-//         return "You Lose! Scissors beats Paper";
-//     } else 
-//     if (playerSelection === computerSelection) {
-//         return "it's a tie!"
-//     } else {
-//         return "huh?";
-//     }
-
-// }
 
 function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
@@ -66,7 +31,6 @@ function playRound(playerSelection, computerSelection){
     } 
 }
 
-//create game funciton
 function game(){
 
     let playerScore = 0;
